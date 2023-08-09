d1c47c2c66a7e2ff078cc77f2ba91b53f8ae91b2
Heather McNeil <hd_mcneil@yahoo.com>
2023-08-08 18:50:49 -0700

    Create challenge7.rb file and complete challenge

---
+++ challenge7.rb
@@ -0,0 +1,27 @@
+# Goals - write a method
+
+# Psuedocode
+# Define method(annual_income)
+#if < 30k = 0, elsif >= 30k && <= 125k = 20%, elseif  >125k && <= 500k = 30%
+#else 40%
+# Use the conditional && in elsif statment
+#call taxes due with value of 'income'
+#store calculated taxes due in a variable called taxes
+# print out statment stating how much is owed using interpolating to
+#insert the amount owed into the statement.
+
+def taxes_due(annual_income)
+    if annual_income < 30000 
+        taxes = 0
+    elsif annual_income >= 30000 && annual_income <= 125000 
+        taxes = annual_income * 0.2
+    elsif annual_income > 125000 && annual_income <= 500000 
+        taxes = annual_income * 0.3
+    else 
+        taxes = annual_income * 0.4
+        end
+    end
+
+    taxes = taxes_due(68000)
+   
+    puts "The total amount you owe in taxes is #{taxes}."
